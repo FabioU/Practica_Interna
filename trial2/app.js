55e952317958d3ef9c8531724758d39dba1262e2
Leonardo Gonzales <leocris26@hotmail.com>
2017-11-25 11:48:04 -0400

    database connection

--- trial2/app.js
+++ trial2/app.js
@@ -11,6 +11,17 @@ var testtable = require('./routes/testtable');
 var tournaments = require('./routes/tournaments');
 var examples = require('./routes/example');
 
+
+var mysql = require('mysql');
+var	myConnection = require ('express-myconnection');
+var dbOptions = {
+		host: 'localhost',
+		user: 'root',
+		password: 'Leopardo26',
+		port: 3306,
+		database: 'intern'
+	};
+
 var app = express();
 
 // view engine setup
@@ -18,18 +29,21 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(myConnection(mysql, dbOptions, 'pool'));
+
 app.use('/', index);
 app.use('/users', users);
 app.use('/testtable', testtable);
 app.use('/torneos', tournaments);
 app.use('/ejemplo', examples);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- trial2/routes/testtable.js
+++ trial2/routes/testtable.js
@@ -1,26 +1,16 @@
 var express = require('express');
 var router = express.Router();
-var mysql = require('mysql');
-
-
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Leopardo26",
-  database: "intern"
-});
-
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  var sql = "SELECT * FROM DISCIPLINA ";
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-    res.render('testtable', { data:result });
-});
+  req.getConnection(function(err, connection) {
+      if (err) return next(err);
+      
+      connection.query('SELECT * FROM DISCIPLINA', function(err, results) {
+        if (err) return next(err);
+        res.render('testtable', { data: results });
+      });
+  
 });
 });
 module.exports = router;
