2ac6498f367e8a96b77c8153e72590d522273ec1
Leonardo Gonzales <leocris26@hotmail.com>
2017-11-26 19:58:55 -0400

    doing stuff

--- trial2/routes/create.js
+++ trial2/routes/create.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var router = express.Router();
-
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('create', function(err, html) {
-  res.send(html);
-	});
+
+router.get('/create?disc*', function (req,res,next){
+	console.log("hi");
+	next();
 });
+router.get('/create', function(req, res, next) {
+
+  res.render('create');
+});
+
+
 module.exports = router;
+
