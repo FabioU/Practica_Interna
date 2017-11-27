9074bd16f646ee31ffe652de34c49824cecb5aa1
Leonardo Gonzales <leocris26@hotmail.com>
2017-11-26 22:31:58 -0400

    Insert into The Database (Only teams  by the moment)

--- trial2/routes/create.js
+++ trial2/routes/create.js
@@ -2,13 +2,27 @@ var express = require('express');
 var router = express.Router();
 /* GET users listing. */
 
-router.get('/create?disc*', function (req,res,next){
-	console.log("hi");
-	next();
-});
-router.get('/create', function(req, res, next) {
+router.get('/', function(req, res, next) {
 
-  res.render('create');
+
+	if (req.query.discT){
+		var v_disc = "'"+(req.query.discT)+"'";
+		var v_cantJug = "'"+(req.query.numberT)+"'";
+		var v_nom = "'"+req.query.nombreT+"'";
+		var v_col ="'"+req.query.colorT+"'";
+			req.getConnection(function(err, connection) {
+	      if (err) return next(err);
+	      
+	      return connection.query('INSERT INTO equipo (cod_Disciplina, cant_Jugadores, nombre, Color) VALUES ('+v_disc+', '+v_cantJug+','+v_nom+','+v_col+');', function(err, results) {
+	        if (err) return next(err);
+	        
+	      });
+  
+		});
+				
+	}
+	res.render('create');
+  	
 });
 
 
